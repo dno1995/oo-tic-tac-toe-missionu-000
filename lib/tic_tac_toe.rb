959793197a8b2fa2aca114da8106826e744d57e5
Dane Olsen <dno1995@gmail.com>
2018-11-20 20:24:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,19 +66,19 @@ class TicTacToe
   #     && @board[win_combination[0]] != ' ')
   #   end
   # end
-  # 
+  #
   # def full?
   #   @board.none?{|spot| spot == " "}
   # end
-  # 
+  #
   # def draw?
   #   won?.nil? && full? == true
   # end
-  # 
+  #
   # def over?
   #   !won?.nil? || draw? == true
   # end
-  # 
+  #
   # def winner
   #   if won?.nil?
   #     won?
@@ -86,12 +86,12 @@ class TicTacToe
   #     @board[won?[0]]
   #   end
   # end
-  # 
+  #
   # def play
   #   until over? == true
   #     turn
   #   end
-  # 
+  #
   #   if !winner.nil?
   #     puts "Congratulations #{winner(@board)}!"
   #   else
