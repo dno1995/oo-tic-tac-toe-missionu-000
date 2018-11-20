027cd2b6f4d7eeb290df808fc6de8d1c36db7e56
Dane Olsen <dno1995@gmail.com>
2018-11-20 20:18:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,11 +20,11 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  
+
   def position_taken?(index)
     @board[index] != '' && @board[index] != ' ' && @board[index] != nil
   end
-  
+
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
